3cbb2b4e482501a88b0ba9e98ad40eeccb564b39
Ayden McClurg <amccoder07@gmail.com>
2020-09-23 17:55:01 -0700

    finished

--- temLit.js
+++ temLit.js
@@ -4,28 +4,72 @@
 
 ////////////////////////Pt. 1////////////////////////
 // create a var that accepts a first name as a prompt
+// let firstName = prompt("Please enter your first name");
+// firstName;
 
+// // create a var that accepts a last name as a prompt
+// let lastName = prompt("Please enter your last name")
+// lastName;
+
+// // Create a variable that will hold a score and a var that will hold a full name 
+// let fullName = firstName + " " + lastName;
+// let score = "7";
+
+// // create a var that adds the full name var and score in a message
+// let finalMes = fullName + " has a score of" + " " + score;
+
+// ////////////////////////Pt. 2////////////////////////
+// //comment out the lines above then create two var's that hold numbers
+// let numOne = "2";
+// let numTwo = "1";
+
+// //create an add var that adds both vars
+// let add = numOne + numTwo;
+
+// //console log the newly created add var
+// console.log(add);
+
+// //console log 3 string's using concatination that say "concatinating is easy""when using template literals,""I can even embed numbers and not have to worry about spacing!"
+// let mes = "concatinating is easy" + " when using template literals," + " I can even embed numbers and not have to worry about spacing!";
+
+// //console log the three strings above and add a third string that includes the add var
+// console.log(`concatinating is easy +  when using template literals, +  I can even embed numbers and not have to worry about spacing! + ${addNum}`);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Name:
+// Date:
+// Assignment: Template Literal Practice
+
+////////////////////////Pt. 1////////////////////////
+// create a var that accepts a first name as a prompt
+let firstName = prompt("Please enter your first name");
+firstName;
 
 // create a var that accepts a last name as a prompt
-
+let lastName = prompt("Please enter your last name")
+lastName;
 
 // Create a variable that will hold a score and a var that will hold a full name 
-
+let fullName = firstName + " " + lastName;
+let score = "7";
 
 // create a var that adds the full name var and score in a message
-
+let finalMes = fullName + " has a score of" + " " + score;
 
 ////////////////////////Pt. 2////////////////////////
 //comment out the lines above then create two var's that hold numbers
-
+let numOne = 2;
+let numTwo = 1;
 
 //create an add var that adds both vars
-
+let add = numOne + numTwo;
 
 //console log the newly created add var
-
+console.log(add);
 
 //console log 3 string's using concatination that say "concatinating is easy""when using template literals,""I can even embed numbers and not have to worry about spacing!"
-
+let mes = `concatinating is easy when using template literals, I can even embed numbers and not have to worry about spacing!`;
 
 //console log the three strings above and add a third string that includes the add var
+console.log(`concatinating is easy when using template literals, I can even embed numbers and not have to worry about spacing! ${add}`);
